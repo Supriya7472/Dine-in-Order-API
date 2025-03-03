831893aea3ff9569db8aeb9c038b7fa03d6d8350
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-03 15:50:58 +0530

    Modifying RegistrationRequest.java by adding input validation

--- src/main/java/com/example/dio/dto/request/RegistrationRequest.java
+++ src/main/java/com/example/dio/dto/request/RegistrationRequest.java
@@ -1,22 +1,36 @@
 package com.example.dio.dto.request;
 
 import com.example.dio.enums.UserRole;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegistrationRequest {
-
+    @NotNull(message = "User name cannot be null")
+    @NotBlank(message = "User name cannot be blank")
+    @Pattern(regexp = "^[a-zA-Z0-9_]$",message = "User name can only contain Alphabets,Numbers and Underscore")
     private String userName;
 
+    @NotNull(message = "email cannot be null")
+    @NotBlank(message = "email cannot be blank")
+    @Email(regexp = "^[A-Za-z0-9._%+-]+@gmail\\.com$",message = "Enter valid email id")
     private String email;
 
+    @NotNull(message = "password cannot be null")
+    @NotBlank(message = "password cannot be blank")
+    @Pattern(regexp = "^[A-Za-z\\d@#$%^&+=!]{8,12}$",message = "Password must contain atleast one lower case, upper case, one number,special character,minlength=8 and maxlength=12")
     private String password;
 
+    @NotNull(message = "Phone number cannot be null")
+    @NotBlank(message = "Phone number cannot be blank")
+    @Pattern(regexp = "^[7-9]d{9}$",message = "Invalid phone number")
     private String phNo;
 
     private UserRole role;
