c59d196248c46a6c16be28769788ec865286b1ea
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-03 13:13:46 +0530

    Modifing UserController.java file

---
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.example.dio.controller;
+
+import com.example.dio.dto.request.RegistrationRequest;
+import com.example.dio.dto.request.UserRequest;
+import com.example.dio.dto.response.UserResponse;
+import com.example.dio.service.UserService;
+import com.example.dio.util.ResponseBuilder;
+import com.example.dio.util.ResponseStructure;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody RegistrationRequest registrationRequest){
+        UserResponse userResponse=userService.registerUser(registrationRequest);
+        return ResponseBuilder.success(HttpStatus.CREATED, "User Created", userResponse);
+
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<ResponseStructure<UserResponse>> findUserById(@PathVariable long userId){
+        UserResponse userResponse=userService.findUserById(userId);
+        return ResponseBuilder.success(HttpStatus.OK, "User Found", userResponse);
+    }
+
+    @PutMapping("/users/{userId}")
+    public ResponseEntity<ResponseStructure<UserResponse>> updateUserById(@RequestBody UserRequest userRequest, @PathVariable long userId){
+        UserResponse userResponse=userService.updateUserById(userRequest,userId);
+        return ResponseBuilder.success(HttpStatus.OK,"User Updated",userResponse);
+    }
+
+}
