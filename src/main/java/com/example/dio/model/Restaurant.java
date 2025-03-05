42e5149606724cdd334a7f05634bf164450ce9d2
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-05 15:42:18 +0530

    Adding Restaurant entity

---
+++ src/main/java/com/example/dio/model/Restaurant.java
@@ -0,0 +1,53 @@
+package com.example.dio.model;
+
+import com.example.dio.enums.DietType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "restaurants")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
+public class Restaurant {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "restaurantId")
+    private long restaurantId;
+
+    @Column(name="restaurantName")
+    private String restaurantName;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name="phNo")
+    private String phNo;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name = "opensAt")
+    private LocalTime opensAt;
+
+    @Column(name = "closesAt")
+    private LocalTime closesAt;
+
+    @Column(name="dietTYpe")
+    @Enumerated(EnumType.STRING)
+    private DietType dietType;
+
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+
+    @Column(name="lastModifiesAt")
+    private LocalDateTime lastModifiedAt;
+
+    @ManyToMany(mappedBy = "restaurant",fetch = FetchType.EAGER)
+    private Cuisine cuisine;
+
+}
