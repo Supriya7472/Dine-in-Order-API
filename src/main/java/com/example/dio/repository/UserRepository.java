c65b4db5709553cdb33d772119e3f17ea4f6415c
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-05 14:19:44 +0530

    Modifying

--- src/main/java/com/example/dio/repository/UserRepository.java
+++ src/main/java/com/example/dio/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.example.dio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-//    User save(User user);
+
 }
