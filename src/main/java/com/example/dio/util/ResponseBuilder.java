d6f79a7c40b28254d80633c49e0a52373dcad31a
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-05 14:25:21 +0530

    Modifying - Providing documentation

--- src/main/java/com/example/dio/util/ResponseBuilder.java
+++ src/main/java/com/example/dio/util/ResponseBuilder.java
@@ -5,6 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseBuilder {
+
+    /**
+     * Helps creating the success responses with data including
+     * the HttpStatus code, message and the data itself.
+     *
+     * @param  status Specifies HttpStatus for succes
+     * @param message Specifies success message
+     * @param data Specifies success data
+     *
+     * @Returns ResponseEntity of type ResponseStructure of type T
+     * */
     public static <T>ResponseEntity<ResponseStructure<T>> success(HttpStatus status, String message , T data){
         ResponseStructure<T> structure=ResponseStructure.<T>builder()
                 .status(status.value())
@@ -16,14 +27,46 @@ public class ResponseBuilder {
                 .body(structure);
 
     }
+
+    /**
+     * Helps to provide status code(OK) for general operations using success method
+     * Acts as factory method for success method
+     *
+     * @param message success message
+     * @param data response data
+     *
+     * @Returns ResponseEntity of type ResponseStructure of type T
+     * */
     public static <T>ResponseEntity<ResponseStructure<T>> ok(String message , T data){
         return success(HttpStatus.OK,message,data);
     }
 
+
+    /**
+     * Helps to provide status code(created) for general operations using success method
+     * Acts as factory method for success method
+     *
+     * @param message success message
+     * @param data response data
+     *
+     * @Returns ResponseEntity of type ResponseStructure of type T
+     * */
     public static <T>ResponseEntity<ResponseStructure<T>> created(String message , T data){
         return success(HttpStatus.CREATED,message,data);
     }
 
+
+    /**
+     * Helps creating the success responses with data including
+     * the HttpStatus code, message and the data itself.
+     *
+     * @param  status Specifies HttpStatus for succes
+     * @param message Specifies success message
+     * @param data Specifies success data
+     * @param headers specifies headers
+     *
+     * @Returns ResponseEntity of type ResponseStructure of type T
+     * */
 
     public static <T> ResponseEntity<ResponseStructure<T>> success(HttpStatus status, HttpHeaders headers,String message,T data){
         ResponseStructure<T> structure=ResponseStructure.<T>builder()
@@ -37,6 +80,14 @@ public class ResponseBuilder {
                 .body(structure);
     }
 
+    /**
+     * Helps creating the error responses with HttpStatus code and message.
+     *
+     * @param  status -> Specifies HttpStatus for error
+     * @param  message -> Specifies error message
+     *
+     * @return ResponseEntity of type SimpleErrorResponse
+     * */
     public static ResponseEntity<SimpleErrorResponse> error(HttpStatus status,String message){
         SimpleErrorResponse error=SimpleErrorResponse.builder()
                 .type(status.name())
@@ -49,6 +100,15 @@ public class ResponseBuilder {
 
     }
 
+
+    /**
+     * Helps to provide status code(created) for general operations using success method
+     * Acts as factory method for success method
+     *
+     * @param message success message
+     *
+     * @Returns ResponseEntity of type ResponseStructure of type SimpleErrorResponse
+     * */
     public static ResponseEntity<SimpleErrorResponse> notFound(String message){
         return error(HttpStatus.NOT_FOUND,message);
     }
