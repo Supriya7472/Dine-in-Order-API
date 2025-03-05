62690505a790a89fdfc9b3abbe40ea231a7c1396
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-05 14:20:33 +0530

    Modifying - Providing documentation

--- src/main/java/com/example/dio/service/UserService.java
+++ src/main/java/com/example/dio/service/UserService.java
@@ -6,9 +6,30 @@ import com.example.dio.dto.response.UserResponse;
 import com.example.dio.model.User;
 
 public interface UserService {
+
+    /**
+     * This service is registers new users
+     *
+     * @param  registrationRequest  Specifies registrationRequest object of type RegistrationRequest
+     *
+     * @return UserResponse
+     * */
     public UserResponse registerUser(RegistrationRequest registrationRequest);
 
+    /**
+     * This service fetches user data using user id
+     *
+     * @param userId specifies user id
+     *
+     * @return UserResponse
+     * */
     public UserResponse findUserById(long userId);
 
+    /**
+     * this service updates existing users
+     *
+     * @param userId specifies user id
+     * @param userRequest specifies userRequest of type UserRequest
+     * */
     public UserResponse updateUserById(UserRequest userRequest, long userId);
 }
