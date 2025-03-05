69a0d53932bdc8640f0da7b8bf9d2f6275a4c683
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-05 14:18:25 +0530

    Modifying - Providing documentation

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody @Valid RegistrationRequest registrationRequest){
         UserResponse userResponse=userService.registerUser(registrationRequest);
-        return ResponseBuilder.success(HttpStatus.CREATED, "User Created", userResponse);
+        return ResponseBuilder.created("User Created", userResponse);
 
     }
 
@@ -61,7 +61,7 @@ public class UserController {
     @GetMapping("/users/{userId}")
     public ResponseEntity<ResponseStructure<UserResponse>> findUserById(@PathVariable long userId){
         UserResponse userResponse=userService.findUserById(userId);
-        return ResponseBuilder.success(HttpStatus.OK, "User Found", userResponse);
+        return ResponseBuilder.ok("User Found", userResponse);
     }
 
 
@@ -78,7 +78,7 @@ public class UserController {
     @PutMapping("/users/{userId}")
     public ResponseEntity<ResponseStructure<UserResponse>> updateUserById(@RequestBody @Valid UserRequest userRequest, @PathVariable long userId){
         UserResponse userResponse=userService.updateUserById(userRequest,userId);
-        return ResponseBuilder.success(HttpStatus.OK,"User Updated",userResponse);
+        return ResponseBuilder.ok("User Updated",userResponse);
     }
 
 }
