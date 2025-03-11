bd0850d85d6ab8816b579ff87be7ae901643becd
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-11 16:40:19 +0530

    adding FoodItemResponse.java

---
+++ src/main/java/com/example/dio/dto/response/FoodItemResponse.java
@@ -0,0 +1,33 @@
+package com.example.dio.dto.response;
+
+import com.example.dio.enums.DietType;
+import com.example.dio.model.Category;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FoodItemResponse {
+
+    private String foodName;
+
+    private double price;
+
+    private String description;
+
+    private int stock;
+
+    private String availability;
+
+    private DietType dietType;
+
+
+
+
+
+}
