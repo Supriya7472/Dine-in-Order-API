3ed4443691c3a383bdeff09ca3eb45b0d021aa0b
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-11 16:03:24 +0530

    Modified Cuisine.java

--- src/main/java/com/example/dio/model/Cuisine.java
+++ src/main/java/com/example/dio/model/Cuisine.java
@@ -1,6 +1,7 @@
 package com.example.dio.model;
 
 import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,4 +19,7 @@ public class Cuisine {
 
     @ManyToMany(mappedBy = "cuisines",fetch = FetchType.EAGER)
     private List<Restaurant> restaurants;
+
+    @OneToMany(mappedBy = "cuisine")
+    private List<FoodItem> foodItems;
 }
