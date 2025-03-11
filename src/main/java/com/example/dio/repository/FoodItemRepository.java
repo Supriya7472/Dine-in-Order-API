ef7c782143c7901dd5bae4cb6bd68c53991f04c6
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-11 16:36:29 +0530

    Adding FoodItemRepository.jav

---
+++ src/main/java/com/example/dio/repository/FoodItemRepository.java
@@ -0,0 +1,20 @@
+package com.example.dio.repository;
+
+import com.example.dio.model.FoodItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FoodItemRepository extends JpaRepository<FoodItem,Long> {
+
+    @Query("SELECT fi FROM FoodItem fi JOIN fi.categories c " +
+            "WHERE c.category IN :categories " +
+            "GROUP BY fi " +
+            "HAVING COUNT(DISTINCT c.category) = :categoryCount")
+    List<FoodItem> findFoodItemsByCategoryNames(@Param("categories") List<String> categories,
+                                                @Param("categoryCount") Long categoryCount);
+}
