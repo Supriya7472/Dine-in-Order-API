afa9a3edae7791e489786ad041d6a85c363702c7
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-11 16:17:48 +0530

    Added RestaurantTable.java

---
+++ src/main/java/com/example/dio/model/RestaurantTable.java
@@ -0,0 +1,35 @@
+package com.example.dio.model;
+
+import com.example.dio.enums.RestaurantTableStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tables")
+@Getter
+@Setter
+
+public class RestaurantTable {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "tableId")
+    private long tableId;
+
+    @Column(name = "tableNo")
+    private int tableNo;
+
+    @Column(name = "tableCapacity")
+    private int tableCapacity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tableStatus")
+    private RestaurantTableStatus tableStatus;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+//    @ManyToMany
+//    private List<Staff> staff;
+
+}
