45a2a4963828e74ad201d78da6d21994b6ea9e77
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-13 22:08:51 +0530

    Modified CartItemRepository.java, added custom JPA methods for updating isOrdered,quantity,findCartItems with isOrdered is false

--- src/main/java/com/example/dio/repository/CartItemRepository.java
+++ src/main/java/com/example/dio/repository/CartItemRepository.java
@@ -4,8 +4,11 @@ import com.example.dio.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
@@ -13,4 +16,13 @@ public interface CartItemRepository extends JpaRepository<CartItem,Long> {
     @Transactional
     @Query("UPDATE CartItem c SET c.quantity = :newQuantity WHERE c.id = :cartItemId")
     int updateQuantityByCartItemId(Long cartItemId, int newQuantity);
+
+
+    List<CartItem> findByIsOrderedAndRestaurantTable_TableId(boolean False, Long tableId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE CartItem c SET c.isOrdered = true WHERE c.id IN :cartItemIds")
+    void updateCartItemsIsOrdered(@Param("cartItemIds") List<Long> cartItemIds);
 }
+
