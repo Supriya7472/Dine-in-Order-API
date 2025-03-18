3a06b55da3ea09a0243fe101dedd1fd0e8213ea4
Supriya7472 <supriyashankar7472@gmail.com>
2025-03-18 13:09:42 +0530

    Adding AppEnv.java, to centralize Cloudinary configuration

---
+++ src/main/java/com/example/dio/config/AppEnv.java
@@ -0,0 +1,23 @@
+package com.example.dio.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "app")
+@Component
+@Getter
+@Setter
+public class AppEnv {
+    private String baseUrl;
+    private Cloudinary cloudinary;
+
+    @Getter
+    @Setter
+    public static class Cloudinary{
+        private String apiKey;
+        private String apiSecret;
+        private String cloudName;
+    }
+}
